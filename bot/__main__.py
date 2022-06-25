79d1a2d8eb912b330374b99b8dc3c776bad593a1
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-06-25 22:00:01 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -57,7 +57,7 @@ def stats(update, context):
             f'<b>├  💽 DISK : {disk}%</b>\n' \
             f'<b>│</b>\n' \
             f'<b>╰──「 🚸 @sk_mass_king 🚸 」</b>'
-    sendMessage(stats, context.bot, update.message)
+    update.effective_message.reply_photo('https://pbs.twimg.com/profile_banners/1003909132796116992/1607060050/1500x500', stats, parse_mode=ParseMode.HTML)
 
 
 def start(update, context):
