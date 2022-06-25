c0ce2345b089ab6cddc749570d0bd3dd4298fb1f
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-06-25 22:34:26 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,7 +56,7 @@ def stats(update, context):
             f'<b>├  🎮 RAM : {mem_p}%</b>\n' \
             f'<b>├  💽 DISK : {disk}%</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>╰──「 🚸 @sk_mass_king 'https://pbs.twimg.com/profile_banners/1003909132796116992/1607060050/1500x500' 🚸 」</b>'
+            f'<b>╰──「 🚸 @sk_mass_king 🚸 」</b>'
     sendMessage(stats, context.bot, update.message)
 
     
