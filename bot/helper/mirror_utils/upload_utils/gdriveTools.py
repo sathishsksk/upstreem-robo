c79c28ba99c52ba786b1287e9209649be65fcfff
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-06-27 10:29:56 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -687,19 +687,19 @@ class GoogleDriveHelper:
             mime_type = meta.get('mimeType')
             if mime_type == self.__G_DRIVE_DIR_MIME_TYPE:
                 self.__gDrive_directory(meta)
-                msg += f'<b>Name: </b><code>{name}</code>'
-                msg += f'\n\n<b>Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                msg += '\n\n<b>Type: </b>Folder'
-                msg += f'\n<b>SubFolders: </b>{self.__total_folders}'
+                msg += f'<b>☞ 🗃Name: </b><code>{name}</code>'
+                msg += f'\n\n<b>☞ 📦Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                msg += '\n\n<b>☞ 🌀Type: </b>Folder'
+                msg += f'\n<b>☞ 📂SubFolders: </b>{self.__total_folders}'
             else:
-                msg += f'<b>Name: </b><code>{name}</code>'
+                msg += f'<b>☞ 🗃Name: </b><code>{name}</code>'
                 if mime_type is None:
                     mime_type = 'File'
                 self.__total_files += 1
                 self.__gDrive_file(meta)
-                msg += f'\n\n<b>Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                msg += f'\n\n<b>Type: </b>{mime_type}'
-            msg += f'\n<b>Files: </b>{self.__total_files}'
+                msg += f'\n\n<b>☞ 📦Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                msg += f'\n\n<b>☞ 🌀Type: </b>{mime_type}'
+            msg += f'\n<b>☞ 📂Files: </b>{self.__total_files}'
         except Exception as err:
             if isinstance(err, RetryError):
                 LOGGER.info(f"Total Attempts: {err.last_attempt.attempt_number}")
