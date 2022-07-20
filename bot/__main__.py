e2c26145a84686696847881b9cad9dde1dee630a
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2022-07-20 12:27:29 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,7 +64,7 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton("⚡Index⚡", "https://sathishsk.skmassking.workers.dev/0:/")
-    buttons.buildbutton("🔥Owner🔥", "https://t.me/sk_mass_king")
+    buttons.buildbutton("🔥Owner🔥", "https://t.me/Sathish_Sk_Offl")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
